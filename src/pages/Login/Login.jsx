d91fd8d97cb3d910a1b8808e2b88559bd543d0c6
umesh604@gmail.com <umesh604@gmail.com>
2024-08-21 08:43:15 +0530

    create login page ui

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Login.css'
+import assets from '../../assets/assets'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className='login'>
+      <img src={assets.logo_big} alt="" className="logo" />
+      <form className="login-form">
+        <h2>Sign Up</h2>
+        <input type="text" placeholder='Username' className='form-input' required />
+        <input type="email" placeholder='Email' className='form-input' required />
+        <input type="password" placeholder='Password' className='form-input' required />
+        <button type='submit'>Sign Up</button>
+        <div className="login-term">
+            <input type="checkbox" />
+            <p>Agree to the terms of use & privacy Policy.</p>
+        </div>
+        <div className="login-forgot">
+            <p className="login-toggle">Already have an Account <span>Click here</span></p>
+        </div>
+      </form>
     </div>
   )
 }
