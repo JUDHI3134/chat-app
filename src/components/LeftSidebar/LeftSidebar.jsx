fb29e3b640f9de288c5898b037eba246562299b7
umesh604@gmail.com <umesh604@gmail.com>
2024-08-21 13:44:43 +0530

    create left sidebar ui

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './LeftSidebar.css'
+import assets from '../../assets/assets'
+
+const LeftSidebar = () => {
+  return (
+    <div className='ls'>
+      <div className="ls-top">
+        <div className="ls-nav">
+            <img src={assets.logo} alt="" className="logo" />
+            <div className="menu">
+                <img src={assets.menu_icon} alt="" />
+            </div>
+        </div>
+
+        <div className="ls-search">
+            <img src={assets.search_icon} alt="" />
+            <input type="text" placeholder='Search here' />
+        </div>
+      </div>
+
+      <div className="ls-list">
+        {Array(12).fill("").map((item,index)=>(
+          <div key={index} className="friends">
+            <img src={assets.profile_img} alt="" />
+            <div>
+                <p>Judhistir behera</p>
+                <span>Hello How are You</span>
+            </div>
+        </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default LeftSidebar
