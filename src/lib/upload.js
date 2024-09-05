4fece96e4cc0e76f027f1323c96a3ce29f46c0c5
umesh604@gmail.com <umesh604@gmail.com>
2024-09-05 15:32:45 +0530

    add profile update search and more functionality

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -7,31 +7,33 @@ const storageRef = ref(storage, `images/${Date.now() + file.name}`);
 
 const uploadTask = uploadBytesResumable(storageRef, file);
 
-
-uploadTask.on('state_changed', 
-  (snapshot) => {
-    
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
+return new Promise((resolve,reject)=>{
+  uploadTask.on('state_changed', 
+    (snapshot) => {
+      
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      }
+    }, 
+    (error) => {
+      
+    }, 
+    () => {
+      // Handle successful uploads on complete
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          resolve(downloadURL);
+      });
     }
-  }, 
-  (error) => {
-    
-  }, 
-  () => {
-    // Handle successful uploads on complete
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        resolve(downloadURL);
-    });
-  }
-);
+  );
+})
+
 }
 
 export default upload;
